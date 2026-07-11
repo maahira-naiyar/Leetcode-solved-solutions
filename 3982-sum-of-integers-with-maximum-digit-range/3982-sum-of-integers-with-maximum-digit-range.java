56b3577148195800640f03dc3b3fd8b18e7ca9fe
maahira-naiyar <maahiranaiyar05@gmail.com>
2026-07-11 15:30:28 +0530

    Time: 1 ms (100%), Space: 45.4 MB (49.36%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.java
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.java
@@ -1,21 +1,52 @@
+// class Solution {
+//     public int maxDigitRange(int[] nums) {
+//         int ans = 0, mx = 0;
+//         for (int x : nums) {
+//             int a = 10, b = 0;
+//             for (int y = x; y > 0; y /= 10) {
+//                 int v = y % 10;
+//                 a = Math.min(a, v);
+//                 b = Math.max(b, v);
+//             }
+//             int r = b - a;
+//             if (mx < r) {
+//                 mx = r;
+//                 ans = x;
+//             } else if (mx == r) {
+//                 ans += x;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxDigitRange(int[] nums) {
-        int ans = 0, mx = 0;
-        for (int x : nums) {
-            int a = 10, b = 0;
-            for (int y = x; y > 0; y /= 10) {
-                int v = y % 10;
-                a = Math.min(a, v);
-                b = Math.max(b, v);
+        int maxRange = -1;
+        int sum = 0;
+
+        for (int num : nums) {
+            int original = num;
+            int max = 0;
+            int min = 9;
+
+            while (num > 0) {
+                int digit = num % 10;
+                max = Math.max(max, digit);
+                min = Math.min(min, digit);
+                num /= 10;
             }
-            int r = b - a;
-            if (mx < r) {
-                mx = r;
-                ans = x;
-            } else if (mx == r) {
-                ans += x;
+
+            int range = max - min;
+
+            if (range > maxRange) {
+                maxRange = range;
+                sum = original;
+            } else if (range == maxRange) {
+                sum += original;
             }
         }
-        return ans;
+
+        return sum;
     }
 }
